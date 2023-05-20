ab7cc1785c56c68afbb2e2199cb7a847dd1ae7d8
MDesu <2dpunk3@mail.ru>
2023-05-20 23:48:36 +0800

    Сделал бд с добавлением и удалением

--- app/src/main/java/com/example/headmanapplication/GroupActivity.java
+++ app/src/main/java/com/example/headmanapplication/GroupActivity.java
@@ -1,20 +1,72 @@
 package com.example.headmanapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SimpleCursorAdapter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import com.example.headmanapplication.DB.DatabaseHelper;
 
 public class GroupActivity extends AppCompatActivity {
 
+    ListView userList;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    SimpleCursorAdapter userAdapter;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
-        TextView textView = new TextView(this);
-        textView.setTextSize(20);
-        textView.setPadding(16, 16, 16, 16);
-        textView.setText("Группа");
-        setContentView(textView);
+
+        userList = findViewById(R.id.list);
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // открываем подключение
+        db = databaseHelper.getReadableDatabase();
+
+        //получаем данные из бд в виде курсора
+        userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE, null);
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[]{DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        userList.setAdapter(userAdapter);
+    }
+
+    // по нажатию на кнопку запускаем UserActivity для добавления данных
+    public void add(View view) {
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Закрываем подключение и курсор
+        db.close();
+        userCursor.close();
     }
 }
--- app/src/main/java/com/example/headmanapplication/ScheduleActivity.java
+++ app/src/main/java/com/example/headmanapplication/ScheduleActivity.java
@@ -14,18 +14,7 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        ArrayList<DayWeek> products = new ArrayList<DayWeek>();
-        if(products.size()==0){
-            products.add(new DayWeek("Понедельник"));
-            products.add(new DayWeek("Вторник"));
-            products.add(new DayWeek("Среда"));
-            products.add(new DayWeek("Четверг"));
-            products.add(new DayWeek("Пятница"));
-            products.add(new DayWeek("Суббота"));
-        }
-        ListView productList = findViewById(R.id.dayweekList);
-        DayAdapter adapter = new DayAdapter(this, R.layout.day_week, products);
-        productList.setAdapter(adapter);
+
     }
 
 }
